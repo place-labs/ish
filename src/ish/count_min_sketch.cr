d6516d043e92f946cdf2d4f3ee8920d43839d861
Kim Burgess <kim@acaprojects.com>
2019-03-11 20:15:03 +1000

    better docs

--- src/ish/count_min_sketch.cr
+++ src/ish/count_min_sketch.cr
@@ -3,9 +3,7 @@ require "math"
 # A Count-Min sketch is a probabilistic data structure used for summarizing
 # streams of data in sub-linear space.
 #
-# Space requirements are bounded and are not dependant on the number of items
-# to be tracked. Instead, performance is modelled by the following two
-# parameters:
+# Performance is modelled by the following two parameters:
 #
 #   epsilon: the error factor
 #     delta: probability of error
@@ -36,8 +34,32 @@ class Ish::CountMinSketch
     end
   end
 
-  # Create a count-min sketch with the specified accuracy characteristics.
-  def initialize(epsilon : Float32 = 0.1, delta : Float32 = 1e-6)
+  # Create a count-min sketch with the specified characteristics.
+  #
+  # When choosing a configuration, you are often trying to minimize the error
+  # term of the estimate. Acceptable errors in estimation fall within a range
+  # which is a factor of *epsilon*. Smaller values of *epsilon* should produce
+  # sketch configurations that provide estimates closer to the true values.
+  # However, this will increase the space required by the sketch.
+  #
+  # Error in estimation is also proportional to the total number of distinct
+  # items counted. A factor in the error term is a summation of total number
+  # of occurences of each distinct item. Another way to reduce the error term
+  # is by counting fewer things.
+  #
+  # Alternatively, you may try to reduce the frequency error and increase the
+  # chance of success. This is modeled as `1.0 - delta`. Increasing this number
+  # can produce sketch configurations with reduced error rates, but that will
+  # require more memory.
+  #
+  # A good rule of thumb for finding a starting configuration is to try an
+  # epsilon with as many significant digits as you have digits in your expected
+  # volume (e.g. given an expected volume of 5000 a good starting point for
+  # epsilon might be `0.0001`).
+  #
+  # An excellent resource that provides visualisation of different
+  # configurations can be found at http://crahen.github.io/algorithm/stream/count-min-sketch-point-query.html
+  def initialize(epsilon : Float32, delta : Float32)
     assert_unit_interval epsilon
     assert_unit_interval delta
 
@@ -47,25 +69,25 @@ class Ish::CountMinSketch
     @sketch = Array.new(depth) { Array.new(width, 0_u32) }
   end
 
-  # Increase the count of an item within the sketch.
+  # Increase the count of *item* within the sketch by *amount* (default 1).
   def increment(item, amount = 1)
     buckets(item).each { |row, i| row.update(i, &.+(amount)) }
     self
   end
 
-  # Retrieve a frequency estimate for an item.
+  # Retrieve a frequency estimate for *item*.
   def count(item)
     buckets(item).map { |row, i| row.fetch i }
                  .min
   end
 
-  # Provide an Iterator with the set of hashes for an item.
+  # Provide an Iterator with the set of hashes for *item*.
   private def hash(item)
     @hash_functions.each.map &.call(item)
   end
 
-  # Given an item, provide an iterator containing each layer of the sketch and
-  # it's associated bucket index.
+  # Given an item, provide an Iterator containing a Tuple of each layer of the
+  # sketch and the associated bucket index.
   private def buckets(item)
     @sketch.each.zip hash(item)
   end
